64817f98fcea437c39ba807d93915188c1e645c9
yahiarrh <yahiarrh@gmail.com>
2024-10-15 16:33:26 +0100

    ..

--- srcs/requirements/wordpress/tools/wp-conf.sh
+++ srcs/requirements/wordpress/tools/wp-conf.sh
@@ -8,7 +8,7 @@ else
     cd /var/www/wordpress
     wp config create --allow-root --dbname=$SQL_DATABASE --dbuser=$SQL_USER --dbpass=$SQL_PASSWORD --dbhost=mariadb --path='/var/www/wordpress'
     wp core install --allow-root --url=$URL --title=$SITE_TITLE --admin_user=$ADMIN_USR --admin_password=$ADMIN_PSWD --admin_email=$ADMIN_EML
-    wp user create $WP_USR $WP_EML --role=subscriber --user_pass=$WP_PS --allow-root
+    wp user create $WP_USR $WP_EML --user_pass=$WP_PS --allow-root
 fi
 
 php-fpm7.4 -F
