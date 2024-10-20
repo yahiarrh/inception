704d227b1726869cb4fe3481c0379c1afd1d11c5
yahiarrh <yahiarrh@gmail.com>
2024-10-20 15:14:27 +0100

    ...

--- srcs/requirements/mariadb/conf/tools/script.sh
+++ srcs/requirements/mariadb/conf/tools/script.sh
@@ -4,7 +4,7 @@ service mariadb start
 
 mariadb -u root -p123 -e "CREATE DATABASE IF NOT EXISTS ${SQL_DATABASE};"
 
-mariadb -u root -p123 -e "CREATE USER IF NOT EXISTS ${SQL_USER}@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
+mariadb -u root -p123 -e "CREATE USER IF NOT EXISTS ${SQL_USER}@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
 
 mariadb -u root -p123 -e "GRANT ALL PRIVILEGES ON ${SQL_DATABASE}.* TO ${SQL_USER}@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
 
